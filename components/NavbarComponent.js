ee9a0a275a07c9eeac4394452e79bb106577d6ed
Rafael Nuansa <iandrewnew@gmail.com>
2023-06-08 02:30:25 +0700

    change navbar

--- components/NavbarComponent.js
+++ components/NavbarComponent.js
@@ -3,9 +3,8 @@ import { AiOutlineHome, AiOutlineCalendar, AiOutlineEnvironment, AiOutlineEdit }
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
 export default function NavbarComponent() {
-
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-zinc-800">
+    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white bg-opacity-60 backdrop-filter backdrop-blur-lg">
       <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
         <ScrollLink
           to="welcome"
@@ -14,10 +13,10 @@ export default function NavbarComponent() {
           spy={true}
           offset={-70}
           type="button"
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-zinc-900 group"
+          className="inline-flex flex-col items-center justify-center px-5 group hover:bg-opacity-80"
         >
-          <AiOutlineHome size={24} className="text-white" />
-          <span className="text-white text-sm">Home</span>
+          <AiOutlineHome size={24} className="" />
+          <span className="text-sm">Home</span>
         </ScrollLink>
         <ScrollLink
           to="schedule"
@@ -26,10 +25,10 @@ export default function NavbarComponent() {
           spy={true}
           offset={-70}
           type="button"
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-zinc-900 group"
+          className="inline-flex flex-col items-center justify-center px-5 group hover:bg-opacity-80"
         >
-          <AiOutlineCalendar size={24} className="text-white" />
-          <span className="text-white text-sm">Schedule</span>
+          <AiOutlineCalendar size={24} className="" />
+          <span className="text-sm">Schedule</span>
         </ScrollLink>
         <ScrollLink
           to="location"
@@ -37,10 +36,10 @@ export default function NavbarComponent() {
           duration={1500}
           spy={true}
           offset={-70}
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-zinc-900 group"
+          className="inline-flex flex-col items-center justify-center px-5 group hover:bg-opacity-80"
         >
-          <AiOutlineEnvironment size={24} className="text-white" />
-          <span className="text-white text-sm">Location</span>
+          <AiOutlineEnvironment size={24} className="" />
+          <span className="text-sm">Location</span>
         </ScrollLink>
         <ScrollLink
           to="wish"
@@ -48,10 +47,10 @@ export default function NavbarComponent() {
           duration={1500}
           spy={true}
           offset={-70}
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-zinc-900 group"
+          className="inline-flex flex-col items-center justify-center px-5 group hover:bg-opacity-80"
         >
-          <AiOutlineEdit size={24} className="text-white" />
-          <span className="text-white text-sm">Wish</span>
+          <AiOutlineEdit size={24} className="" />
+          <span className="text-sm">Wish</span>
         </ScrollLink>
       </div>
     </div>
