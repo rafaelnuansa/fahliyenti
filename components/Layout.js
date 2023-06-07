cea3bead572ff2d6e5f08cb3f04d32a76f544c0e
Rafael Nuansa <iandrewnew@gmail.com>
2023-06-08 01:11:48 +0700

    meta

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 import NavbarComponent from "./NavbarComponent";
 import AudioPlayer from "./AudioPlayer";
-
+import { useRouter } from "next/router";
 export default function Layout({ children }) {
   useEffect(() => {
     // Mulai pemutaran audio saat halaman dimuat
@@ -12,12 +12,21 @@ export default function Layout({ children }) {
     }
   }, []);
 
+  const router = useRouter();
+  const currentURL = router.asPath;
+
   return (
     <>
       <Head>
         <title>Yenti & Fahli Wedding Invitation</title>
         <meta name="description" content="Undangan Pernikahan Yenti & Fahli" />
         <meta name="keywords" content="Invitation, Wedding, Wedding Invitation" />
+        <meta property="twitter:image" content="/bg1.jpg"></meta>
+        <meta property="twitter:card" content="/bg1.jpg"></meta>
+        <meta property="og:image" content="/bg1.jpg"></meta>
+        <meta property="og:title" content="Undangan Pernikahan Yenti & Fahli"></meta>
+        <meta property="og:description" content="Undangan Pernikahan Yenti & Fahli" />
+        <meta property="og:url" content={currentURL}></meta>
       </Head>
       <main>{children}</main>
       <NavbarComponent />
